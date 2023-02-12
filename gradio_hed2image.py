2563cd51314bf5212139a56f5706b5286ac766eb
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-13 02:12:36 +0300

    cuda

--- gradio_hed2image.py
+++ gradio_hed2image.py
@@ -16,7 +16,7 @@ from ldm.models.diffusion.ddim import DDIMSampler
 
 
 model = create_model('./models/cldm_v15.yaml').cpu()
-model.load_state_dict(load_state_dict('./models/control_sd15_hed.pth', location='cpu'))
+model.load_state_dict(load_state_dict('./models/control_sd15_hed.pth', location='cuda'))
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
