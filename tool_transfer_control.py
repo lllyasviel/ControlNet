017a6e950e38e7e45c89fae96f80ebab81dc3ede
lvmin <lyuminzhang@outlook.com>
2023-02-11 20:41:56 -0800

    safetensor

---
+++ tool_transfer_control.py
@@ -0,0 +1,11 @@
+import os
+
+path_sd15 = './models/v1-5-pruned.ckpt'
+path_sd15_with_control = './models/control_sd15_openpose.pth'
+path_input = './models/Anything-V3.0.safetensors'
+path_output = './models/control_any3_openpose.ckpt'
+
+assert os.path.exists(path_sd15), 'Input path_sd15 does not exists!'
+assert os.path.exists(path_sd15_with_control), 'Input path_sd15_with_control does not exists!'
+assert os.path.exists(path_input), 'Input path_input does not exists!'
+assert not os.path.exists(path_output), 'Output path_output already exists!'
