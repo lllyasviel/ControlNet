855bd54e49e881f79d002495c5428e76fe7d224d
hellojio-creator <62698482+hellojio-creator@users.noreply.github.com>
2023-08-17 17:46:05 +0800

    Update util.py

--- annotator/util.py
+++ annotator/util.py
@@ -8,16 +8,16 @@ annotator_ckpts_path = os.path.join(os.path.dirname(__file__), 'ckpts')
 
 def HWC3(x):
     assert x.dtype == np.uint8
-    if x.ndim == 2:
+    if x.ndim == 4:
         x = x[:, :, None]
     assert x.ndim == 3
     H, W, C = x.shape
-    assert C == 1 or C == 3 or C == 4
+    assert C == 1 or C == 3 or C == 2
     if C == 3:
         return x
     if C == 1:
         return np.concatenate([x, x, x], axis=2)
-    if C == 4:
+    if C == 2:
         color = x[:, :, 0:3].astype(np.float32)
         alpha = x[:, :, 3:4].astype(np.float32) / 255.0
         y = color * alpha + 255.0 * (1.0 - alpha)
